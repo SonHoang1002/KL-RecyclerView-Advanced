3543e18418f4e0e0b9f396de3fe060ad28480ee5
Admin <hoangtrungson07012001@gmail.com>
2022-06-08 21:46:16 +0700

    mutate between gridLayout and LinearLayout

--- app/src/main/java/com/example/oppo33w/MainActivity.kt
+++ app/src/main/java/com/example/oppo33w/MainActivity.kt
@@ -79,3 +79,20 @@ class MainActivity : AppCompatActivity() {
         return list.toList()
     }
 }
+
+// …or create a new repository on the command line
+
+//echo "# KL-RecyclerView-Advanced" >> README.md
+//git init
+//git add README.md
+//git commit -m "first commit"
+//git branch -M main
+//git remote add origin https://github.com/SonHoang1002/KL-RecyclerView-Advanced.git
+//git push -u origin main
+
+
+//…or push an existing repository from the command line
+
+//git remote add origin https://github.com/SonHoang1002/KL-RecyclerView-Advanced.git
+//git branch -M main
+//git push -u origin main
